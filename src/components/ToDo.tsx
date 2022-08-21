4af38d800b88977634319def5b6d62bd37dda652
Romuru <pastelblue0721@gmail.com>
2022-08-21 18:15:50 +0900

    [feat] recoil components setting to index.tsx ,  refactoring  category logic

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -12,21 +12,21 @@ function ToDo({ text, category, id }: IToDo) {
   return (
     <li>
       <span>{text}</span>
-      {category !== "DOING" && (
+      {category !== "DOING" ? (
         <button name="DOING" onClick={onClick}>
           Doing
         </button>
-      )}
-      {category !== "TO_DO" && (
+      ) : null}
+      {category !== "TO_DO" ? (
         <button name="TO_DO" onClick={onClick}>
           To Do
         </button>
-      )}
-      {category !== "DONE" && (
+      ) : null}
+      {category !== "DONE" ? (
         <button name="DONE" onClick={onClick}>
           Done
         </button>
-      )}
+      ) : null}
     </li>
   );
 }
